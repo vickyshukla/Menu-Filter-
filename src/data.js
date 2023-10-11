2638596b66592049c2800bbf53e393ffaf136af8
vickyshukla <vikkyshukla19988.com>
2023-10-11 17:23:54 +0530

    Added all files

---
+++ src/data.js
@@ -0,0 +1,49 @@
+const menu = [
+    {
+        id:1,
+        title:'Non Fried Masala Banana Chips',
+        category: 'brunch',
+        price:'30.50',
+        img:'https://i.ndtvimg.com/i/2016-07/banana-chips_625x350_71469430885.jpg',
+        desc:'We all love crispy banana chips which is made with raw banana. Normally these delicious chips are fried but here we bring a healthy version of this favourite snack, you will definitely like this non fried masala banana chips.',
+        reci_detail:'2 Raw Bananas, peeled, 1/4 tsp Red Chili Powder, 1/4 tsp Black Pepper, 1/4 tsp Chaat Masala, to taste Salt, 2 tsp Oil, A pinch of Roasted cumin powder',
+    },
+    {
+        id:2,
+        title:'Masoor Dal Vada',
+        category: 'breakfast',
+        price:'50',
+        img:'https://c.ndtvimg.com/2023-08/71i03qe_vada_625x300_08_August_23.jpg',
+        desc:'Vada is a popular south indian snack, you can find many variations of this. Here we bring another vada recipe for that can be prepare with masoor dal and some spice- This vada can be pair wtih coconut chutney or a hot steaming cup of tea.',
+        reci_detail:'2 Raw Bananas, peeled, 1/4 tsp Red Chili Powder, 1/4 tsp Black Pepper, 1/4 tsp Chaat Masala, to taste Salt, 2 tsp Oil, A pinch of Roasted cumin powder',
+    },
+    {
+        id:3,
+        title:'Coconut and Ding Ladoo',
+        category: 'lunch',
+        price:'50',
+        img:'https://c.ndtvimg.com/2023-08/mgmp22e8_coconut-ladoo_625x300_29_August_23.jpg',
+        desc:'Here we bring another delicious dessert recipe and it is a perfect recipe for Raksha Bandhan.',
+        reci_detail:'2 Raw Bananas, peeled, 1/4 tsp Red Chili Powder, 1/4 tsp Black Pepper, 1/4 tsp Chaat Masala, to taste Salt, 2 tsp Oil, A pinch of Roasted cumin powder',
+    },
+    {
+        id:4,
+        title:'Coconut',
+        category: 'breakfast',
+        price:'50',
+        img:'https://c.ndtvimg.com/2023-08/mgmp22e8_coconut-ladoo_625x300_29_August_23.jpg',
+        desc:'Here we bring another delicious dessert recipe and it is a perfect recipe for Raksha Bandhan.',
+        reci_detail:'2 Raw Bananas, peeled, 1/4 tsp Red Chili Powder, 1/4 tsp Black Pepper, 1/4 tsp Chaat Masala, to taste Salt, 2 tsp Oil, A pinch of Roasted cumin powder',
+    },
+    {
+        id:5,
+        title:'Lunch',
+        category: 'Dinner',
+        price:'50',
+        img:'https://c.ndtvimg.com/2023-08/mgmp22e8_coconut-ladoo_625x300_29_August_23.jpg',
+        desc:'Here we bring another delicious dessert recipe and it is a perfect recipe for Raksha Bandhan.',
+        reci_detail:'2 Raw Bananas, peeled, 1/4 tsp Red Chili Powder, 1/4 tsp Black Pepper, 1/4 tsp Chaat Masala, to taste Salt, 2 tsp Oil, A pinch of Roasted cumin powder',
+    },
+];
+
+export default menu;
